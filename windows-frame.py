097c25d9e437d308199338c10c494e0904e9d507
Zarin Rayhana Leenat <50223492+zarinrayhana@users.noreply.github.com>
2020-10-03 10:50:54 +1000

    Update windows-frame.py

--- windows-frame.py
+++ windows-frame.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, Frame, BOTH
 
-class soft_frame(Frame):
+class frame(Frame):
 
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")   
@@ -14,7 +14,7 @@ def main():
     root = Tk()
     root.geometry("850x500+500+500")
     root.resizable(width=True, height=True)
-    app = soft_frame(root)
+    app = frame(root)
     root.mainloop()  
 
 if __name__ == '__main__':
