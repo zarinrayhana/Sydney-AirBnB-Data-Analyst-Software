c1e34bdc88094527fbf42d540384c1c5fce86c4d
Zarin Rayhana Leenat <50223492+zarinrayhana@users.noreply.github.com>
2020-10-03 10:34:56 +1000

    Add files via upload

---
+++ windows-frame.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, Frame, BOTH
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")   
+
+        self.parent = parent        
+        self.parent.title('Sydney AirBnB Data Analysis & Visualisation Software')
+        self.pack(fill=BOTH, expand=1)
+
+def main():
+
+    root = Tk()
+    root.geometry("850x500+500+500")
+    root.resizable(width=False, height=False)
+    app = Example(root)
+    root.mainloop()  
+
+if __name__ == '__main__':
+    main()
+    
