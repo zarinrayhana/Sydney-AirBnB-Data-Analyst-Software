50bbe84ca51acbfb6a1f640137d9fd1d74d82c63
Rotimi-pixel <70616905+Rotimi-pixel@users.noreply.github.com>
2020-10-04 18:01:48 +1000

    Add files via upload

--- pythonassaignment2.py
+++ pythonassaignment2.py
@@ -1,15 +1,12 @@
 import wx
 
+
+
 class bucky (wx.Frame) :
 
     def __init__(self, parent, id):
         wx.Frame.__init__(self, parent, id, 'System Analysis and Visualisation Tool', size=(1200, 1200))
         panel = wx.Panel(self)
-
-        customtitle=wx.StaticText(panel, -1, "System Analysis and Visualisation Tool",(10,20), (260,10), wx.ALIGN_CENTER)
-        customtitle.SetForegroundColour('white')
-        customtitle.SetSize(1200,20)
-        customtitle.SetBackgroundColour('light blue')
 
         getname = wx.TextEntryDialog(None,"What's you Name", "Title", 'Enter Name')
         if getname.ShowModal()==wx.ID_OK:
@@ -22,7 +19,39 @@ class bucky (wx.Frame) :
         customise.SetForegroundColour('white')
         customise.SetBackgroundColour('light blue')
 
-        wx.CheckBox(panel, -1, "Price Distribution Chart",(10,210), (160,-1))
+        search_Suburb =  wx.Button(panel, -1, "Search Suburb",(10,130), (140,-1))
+        self.Bind(wx.EVT_BUTTON, self.searchbutton, search_Suburb)
+
+        search_keyword = wx.Button(panel, -1, "Search Keyword", (10, 190), (140, -1))
+        self.Bind(wx.EVT_BUTTON, self.keywordbutton, search_keyword)
+
+        search_startperiod = wx.Button(panel, -1, "Select Start Period", (550, 130), (140, -1))
+        self.Bind(wx.EVT_BUTTON, self.startperiodbutton, search_startperiod)
+
+        search_endperiod = wx.Button(panel, -1, "Select End Period", (850, 130), (140, -1))
+        self.Bind(wx.EVT_BUTTON, self.endperiodbutton, search_endperiod)
+
+    def searchbutton(self,event):
+        search = wx.TextEntryDialog(None, "Search Suburb", "Title", "Ashgrove")
+        if search.ShowModal()==wx.ID_OK:
+            answer =search.GetValue()
+
+    def keywordbutton(self, event):
+        searchkey = wx.TextEntryDialog(None, "Search Keyword", "Title", "Pool")
+        if searchkey.ShowModal() == wx.ID_OK:
+            answerkey = searchkey.GetValue()
+
+    def startperiodbutton(self, event):
+        search_startperiod = wx.TextEntryDialog(None, "Select First Period", "Title", "1/09/2018")
+        if search_startperiod.ShowModal() == wx.ID_OK:
+            answerstartperiod = search_startperiod.GetValue()
+
+    def endperiodbutton(self, event):
+        search_endperiod = wx.TextEntryDialog(None, "Select First Period", "Title", "1/09/2018")
+        if search_endperiod.ShowModal() == wx.ID_OK:
+            answerendperiod = search_endperiod.GetValue()
+
+
 
 if __name__ == '__main__':
      app = wx.PySimpleApp()
